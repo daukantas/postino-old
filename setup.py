b280aca7493939689948857a4e4463502c22e84a
Michał Leśniewski <mlesniew@gmail.com>
2016-03-22 22:20:52 +0100

    Add a basic setup.py file

---
+++ setup.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+from setuptools import setup
+
+setup(
+    name='postino',
+    version='0.1',
+    description='Easy email sending',
+    url='http://github.com/mlesniew/postino',
+    author='Michał Leśniewski',
+    author_email='mlesniew@gmail.com',
+    license='GPLv3',
+    packages=['postino'],
+    install_requires=[
+        'markdown',
+        'pyzmail',
+    ],
+    zip_safe=False)
